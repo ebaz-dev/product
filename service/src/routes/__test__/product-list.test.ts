f85bad173039b15d37c107584729bdd37fbc3838
Ganpurev99 <ganpurev.d@ebazaar.mn>
2024-09-23 09:43:09 +0800

    test package josn update

--- service/src/routes/__test__/product-list.test.ts
+++ service/src/routes/__test__/product-list.test.ts
@@ -4,7 +4,7 @@ import { app } from "../../app";
 const apiPrefix = `/api/v1/products`;
 
 it("fails when invalid IDs are provided", async () => {
-  const response = await request(app)
+  await request(app)
     .get(apiPrefix)
     .query({ ids: "invalid-id" })
     .expect(400);
